ab01e21f59c1e64b4ce0fe9e7c9991fcb7076ce8
baa76 <dev@brynroberts.net>
2018-10-06 13:58:43 +1000

    Getting to log range to test return values

--- Indent all but 1st line.js
+++ Indent all but 1st line.js
@@ -1,19 +1,19 @@
 // Action: Change this script to indent all but the first line; this will then be used as a first step in a drafts action paste dump to a new 2do project or checklist
+const allText = draft.content();
 
-
-// character string used for indent
-let indent = "    ";
-if (draft.languageGrammar == "Taskpaper") {
-	indent = "\t";
-}
-
-// grab ranges and text
 let [selStart, selLen] = editor.getSelectedRange();
 let [lnStart, lnLen] = editor.getSelectedLineRange();
 let lnText = editor.getTextInRange(lnStart, lnLen);
 
+console.log(lnText);
+
+// character string used for indent
+// let indent = "    ";
+
+// grab ranges and text
+
 // loop over lines and add indents skipping blank lines
-let indentedLines = [];
+/* let indentedLines = [];
 let indentCt = 0;
 let fl = false;
 if (lnText.endsWith("\n")) {
@@ -39,4 +39,4 @@ editor.setTextInRange(lnStart, lnLen, resultText);
 // update selection
 let newStart = selStart + indent.length;
 let newLen = selLen + (indent.length * (indentCt - 1))
-editor.setSelectedRange(newStart, newLen);
+editor.setSelectedRange(newStart, newLen); */
