19b9be626d29edd5f5f11d71e4cb75196b0c7f08
baa76 <dev@brynroberts.net>
2018-12-06 06:18:39 +1100

    Added button pressed

--- Reminder List Prompt.js
+++ Reminder List Prompt.js
@@ -16,28 +16,26 @@ var p = Prompt.create();
 p.title = "Lists";
 p.message = "Choose a List";
 for (i = 0; i < lists.length; i++) {
-  p.addButton(lists[i]);
-  //p.addTextField("fieldName", "Label", "");
+  p.addButton(lists[i], '#'+ lists[i]);
 }
 var con = p.show();
 
 if (con) {
-  var textToInsert = "insert this at the start: ";
+  var textToInsert = prompt.buttonPressed;
+  var sel = editor.getSelectedText();
+  var selRange = editor.getSelectedRange();
 
-var sel = editor.getSelectedText();
-var selRange = editor.getSelectedRange();
+  draft.content = textToInsert + draft.content;
+  draft.update();
 
-draft.content = textToInsert + draft.content;
-draft.update();
+  editor.activate();
 
-editor.activate();
-
-if (!sel || sel.length == 0) {
-  editor.setSelectedRange(selRange[0]+textToInsert.length,0);
-}
-else {
-  editor.setSelectedRange(selRange[0]+textToInsert.length, selRange[1],0);
-}
+  if (!sel || sel.length == 0) {
+    editor.setSelectedRange(selRange[0]+textToInsert.length,0);
+  }
+  else {
+    editor.setSelectedRange(selRange[0]+textToInsert.length, selRange[1],0);
+  }
 }
 else {
   context.cancel();
